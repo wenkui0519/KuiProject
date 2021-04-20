98c9ba04c525ca52ec4d19505b6ac75d3c6108a2
文魁 <54048522+wenkui0519@users.noreply.github.com>
2021-04-20 17:01:41 +0800

    Update form-editor.component.ts
    wB420控件点击删除操作

--- src/app/module/form-design/component/form-editor/form-editor.component.ts
+++ src/app/module/form-design/component/form-editor/form-editor.component.ts
@@ -71,99 +71,55 @@ export class FormEditorComponent implements OnInit {
                     return controlList;
                 }
             });
-            // // 删除控件
-            // editor.ui.registry.addButton('deleteControl', {
-            //     // title: trans('common.delete'),
-            //     icon: 'close',
-            //     tooltip: this.trans('common.delete'),
-            //     onAction: (e) => {
-            //         const $selectedNode = jQuery(editor.selection.getNode());
-            //         editor.undoManager.add();
-            //         if ($selectedNode.attr('data-efb-control')) {
-            //             const controlId = $selectedNode.attr('id'),
-            //                 controlType = $selectedNode.attr('data-efb-control');
+            // 删除控件
+            editor.ui.registry.addButton('deleteControl', {
+                icon: 'close',
+                tooltip: 'delete',
+                onAction: (e) => {
+                    const $selectedNode = jQuery(editor.selection.getNode());
+                    editor.undoManager.add();
+                    if ($selectedNode.attr('data-efb-control')) {
+                        const controlId = $selectedNode.attr('id'),
+                            controlType = $selectedNode.attr('data-efb-control');
 
-            //             // 移除控件
-            //             // setCursorLocation()要两个参数：node\offset
-            //             tinymce.get('form-editor').selection.setCursorLocation(editor.selection.getNode(), 0);
-            //             $selectedNode.remove();
-            //             editor.insertContent('');
+                        // 移除控件
+                        tinymce.get('form-editor').selection.setCursorLocation(editor.selection.getNode(), 0);
+                        $selectedNode.remove();
+                        editor.insertContent('');
 
-            //             // 移除控件列表中的控件
-            //             if (this.controlList[controlId]) {
-            //                 delete this.controlList[controlId];
+                        this.changeRef.detectChanges();
+                    }
+                    editor.save();
+                    this.changeRef.detectChanges();
 
-            //                 // 移除控件分类列表中的控件
-            //                 if (this.controlListByCategory[controlType] && this.controlListByCategory[controlType]['controlList']
-            //                     && this.controlListByCategory[controlType]['controlList'][controlId]) {
-            //                     delete this.controlListByCategory[controlType]['controlList'][controlId];
-            //                     if (Object.keys(this.controlListByCategory[controlType]['controlList']).length === 0) {
-            //                         delete this.controlListByCategory[controlType];
-            //                     }
-            //                 }
-            //                 this.controlSummaryService.handle$.next({
-            //                     key: 'update',
-            //                     value: this.controlListByCategory
-            //                 });
-            //                 // 删除控件后同步更新form-design的attributeValue
-            //                 this.controlAttributeService.setAttribute$.next('');
-            //                 this.editorService.currentEditArea$.emit('');
-            //             }
-            //             // 更新控件概要列表
-            //             this.formDesignService.summaryControlChange$.emit('update');
+                },
+            });
 
-            //             this.changeRef.detectChanges();
-            //         }
-            //         // 执行这个操作的目的是为了删除控件后让那个×自动消失
-            //         // tinnymce应该有这个方法的，还没找到
-            //         // 如果执行下面这个操作  会滚动到底部
-            //         // tinymce.get('form-editor').selection.getStart().click();
-            //         // 删除了空的明细布局，保存表单还提示存在没有子项的明细
-            //         // 这个方法会增加一次撤销级别
-            //         // editor.undoManager.add();
-            //         // editor.insertContent('');
-            //         // editor.undoManager.undo();
-            //         // editor.setDirty(false);
-            //         // editor.focus(false);
-            //         editor.save();
-            //         this.changeRef.detectChanges();
+            // 单击控件操作
+            editor.on('click', (e) => {
+                this.styleSetService.close$.emit('');
+                const $element = jQuery(e.target);
+              
+                const allElement = jQuery(tinymce.get('form-editor').getBody()).find('*[data-efb-control]');
+                angular.forEach(allElement, (data, index) => {
+                    const newfileCounter = Number(data.id.replace('DATA_', ''));
+                    if (this.attributeService.fieldCounter < newfileCounter) {
+                        this.attributeService.fieldCounter = newfileCounter;
+                    }
+                });
 
-            //     },
-            // });
-
-            // // 单击控件操作
-            // editor.on('click', (e) => {
-            //     this.styleSetService.close$.emit('');
-            //     const $element = jQuery(e.target);
-            //     // 定位到全部标签列表
-            //     this.formDesignService.controlTab$.emit('blur');
-            //     // this.openMenu();
-            //     // 移除表单检查的控件定位
-            //     jQuery(tinymce.get('form-editor').getBody()).find('.control-error[data-efb-control]').removeClass('control-error');
-
-            //     const allElement = jQuery(tinymce.get('form-editor').getBody()).find('*[data-efb-control]');
-
-            //     angular.forEach(allElement, (data, index) => {
-            //         const newfileCounter = Number(data.id && data.id.replace('DATA_', ''));
-            //         if (this.attributeService.fieldCounter < newfileCounter) {
-            //             this.attributeService.fieldCounter = newfileCounter;
-            //         }
-            //     });
-
-            //     // 控件必须有 data-efb-control 属性
-            //     if ($element.attr('data-efb-control')) {
-            //         const controlType = $element.attr('data-efb-control');
-            //         const attributes = this.complexControlService.controlList()[controlType]['attribute'];
-            //         this.ngZone.run(_ => {
-            //             this.controlAttributeService.setAttributeValue$.next($element);
-            //             this.controlAttributeService.setAttribute$.next(attributes);
-            //             this.editorService.currentEditArea$.emit('control');
-            //             this.selectTab.emit(1);
-            //         });
-
-            //     }
-            //     this.changeRef.detectChanges();
-            // });
+                // 控件必须有 data-efb-control 属性
+                if ($element.attr('data-efb-control')) {
+                    const controlType = $element.attr('data-efb-control');
+                    const attributes = this.complexControlService.controlList()[controlType]['attribute'];
+                    this.ngZone.run(_ => {
+                        this.controlAttributeService.setAttributeValue$.next($element);
+                        this.controlAttributeService.setAttribute$.next(attributes);
+                        this.editorService.currentEditArea$.emit('control');
+                    });
+                }
+                this.changeRef.detectChanges();
+            });
             this.changeRef.markForCheck();
         };
     }
